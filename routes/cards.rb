246917a660ca738c01e054c96a054a3681b4d481
mitchelljfs <mitchelljfsimon@gmail.com>
2017-12-25 18:45:58 -0800

    added add card and delete card functionality

--- routes/cards.rb
+++ routes/cards.rb
@@ -10,4 +10,31 @@ class Memebank < Sinatra::Application
       slim :card
     end
   end
+
+  get '/add_card/:bank_id' do
+    retrieve_banks
+    @bank_id = params[:bank_id]
+
+    slim :home, layout: :application, locals: { current_view: :none } do
+      slim :add_card
+    end
+  end
+
+  post '/add_card/:bank_id' do
+    @card = RestClient.post "#{@@api_url}/banks/#{params[:bank_id]}/cards",
+                            { card: { src: params[:src] } },
+                            authorization: 'Authorization: Bearer ' \
+                            "#{session[:user_token]}"
+    @card = JSON.parse @card, symbolize_names: true
+
+    redirect to("/card/#{@card[:id]}")
+  end
+
+  get '/delete_card/:id/:bank_id' do
+    RestClient.delete "#{@@api_url}/cards/#{params[:id]}",
+                      authorization: 'Authorization: Bearer ' \
+                      "#{session[:user_token]}"
+
+    redirect to("/home/#{params[:bank_id]}")
+  end
 end
